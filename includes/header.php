ce66d7ae724c1c7d5dd5296af370631fb7aa6aee
Shiya <shermanshiya@gmail.com>
2025-12-02 23:47:42 -0500

    Merged master into main and resolved conflicts

--- includes/header.php
+++ includes/header.php
@@ -23,6 +23,7 @@ if (empty($categories)) {
 <html lang="en">
 
 <head>
+    
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Shiya's Blog</title>
